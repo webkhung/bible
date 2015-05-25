12893da3d713eb0086e25d186318f4ad2401414a
Kelvin Hung <kelvin_hung@intuit.com>
2015-05-25 00:00:35 -0700

    Trying

--- public/reading_plan.js
+++ public/reading_plan.js
@@ -793,6 +793,12 @@ $( document ).ready(function() {
 
         chrome.webstore.install('https://chrome.google.com/webstore/detail/jogajkcgclkfedbhdcopmpmeeophkkji', handleInstallSuccess, handleInstallFailure);
     });
+
+    $(".rotate").textrotator({
+        animation: "dissolve", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
+        separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
+        speed: 2000 // How many milliseconds until the next word show.
+    });
 });
 
 if (chrome.app.isInstalled) {
